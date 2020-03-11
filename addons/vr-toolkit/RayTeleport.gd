e060ea2a2e9ae03b5e8b9bde57cb8d3b540c539a
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-03-11 15:30:11 +0100

    Improvement of usability of RayTeleport.

--- addons/vr-toolkit/RayTeleport.gd
+++ addons/vr-toolkit/RayTeleport.gd
@@ -2,10 +2,10 @@ extends "res://addons/vr-toolkit/ARVRControllerExtension.gd"
 
 
 export(float) var min_pitch = -80
-export(float) var max_pitch = 90
+export(float) var max_pitch = 130
 export(float) var max_distance = 5000
 export(float) var cast_height = 6
-export(float) var controller_degree_compensation = -50
+export(float) var controller_degree_compensation = -60
 export(Color) var can_teleport = Color.green
 export(Color) var cannot_teleport = Color.red
 
@@ -28,7 +28,6 @@ func _ready():
 	position_indicator.set_material_override(visualizer_material)
 	_init_bezier()
 	horizontal_ray.rotation_degrees.x = controller_degree_compensation
-	tall_ray.global_transform.origin = origin.global_transform.origin + Vector3.UP * cast_height
 
 
 # Because we need to have 3 points to draw the bezier we have to initialize them
@@ -47,9 +46,10 @@ func on_button_pressed(id):
 
 
 func _process(delta):
+	tall_ray.global_transform.origin = origin.global_transform.origin + Vector3.UP * cast_height
 	_find_horizontal_point()
 	_find_cast_position()
-	
+
 	if tall_ray.is_colliding():
 		position_indicator.visible = true
 		position_indicator.global_transform.origin = tall_ray.get_collision_point()
@@ -59,7 +59,7 @@ func _process(delta):
 		position_indicator.visible = false
 		visualizer_material.albedo_color = cannot_teleport
 		visualizer_material.emission = cannot_teleport
-	
+
 	_draw_bezier()
 	_visualize()
 
@@ -67,12 +67,12 @@ func _process(delta):
 # Finds the maximum distance along the horizontal ray
 func _find_horizontal_point():
 	# The controller pitch is somewhat messed up and not quite what you would expect.
-	# This unexpected pitch is compensated by the raycast node itself, thus get the pitch from it. 
-	var controller_pitch = rad2deg(horizontal_ray.global_transform.basis.get_euler().x) 
+	# This unexpected pitch is compensated by the raycast node itself, thus get the pitch from it.
+	var controller_pitch = rad2deg(horizontal_ray.global_transform.basis.get_euler().x)
 	controller_pitch = clamp(controller_pitch, min_pitch, max_pitch) # clamp the wished maximum rotations of the controller
 	# Normalize the current pitch somewhere between 0 (min_pitch) and 1 (max_pitch)
 	var normalized_pitch = inverse_lerp(min_pitch, max_pitch, controller_pitch)
-	
+
 	var horizontal_distance = max_distance * normalized_pitch
 	# The resulting point is the horizontal_distance in pointing direction of the controller away from its origin
 	horizontal_point = horizontal_ray.get_global_transform().origin + -horizontal_ray.get_global_transform().basis.z * horizontal_distance
@@ -82,7 +82,7 @@ func _find_cast_position():
 	# The origin of the tall_ray is the arvr origin + a given height above
 	var cast_position = origin.get_global_transform().origin + Vector3.UP * cast_height
 	var cast_direction = horizontal_point - tall_ray.global_transform.origin
-	
+
 	tall_ray.global_transform.origin = cast_position
 	tall_ray.cast_to = cast_direction
 
@@ -90,28 +90,33 @@ func _find_cast_position():
 func _draw_bezier():
 	var start_pos = controller.get_global_transform().origin
 	var end_pos
+	var mid_pos
+	var direction = (tall_ray.to_global(tall_ray.cast_to) - start_pos).normalized()
+	var forward = -(horizontal_ray.get_global_transform().basis.z)
+	
 	if tall_ray.is_colliding():
 		end_pos = tall_ray.get_collision_point()
 	else:
-		end_pos = tall_ray.to_global(tall_ray.cast_to)
+		end_pos = tall_ray.to_global(tall_ray.cast_to) + Vector3.DOWN * 50
 	
 	var distance = start_pos.distance_to(end_pos)
-	var mid_pos = (end_pos + start_pos) / 2 + Vector3.UP * (distance / 10)  
-	
+	mid_pos = (end_pos + start_pos) / 2 + Vector3.UP * (distance / 10)
+
 	bezier.set_point_position(0, start_pos)
 	bezier.set_point_position(1, mid_pos)
 	bezier.set_point_position(2, end_pos)
 	# Also set the in- and out-point (this makes the bezier effect)
-	var direction = (end_pos - start_pos).normalized()
+	direction = (end_pos - start_pos).normalized()
 	bezier.set_point_in(1, direction * -2)
 	bezier.set_point_out(1, direction * 2)
 
 
+# Give the points of the curve to the ImmediateGeometry-Node which do the visualization
 func _visualize():
 	visualizer.clear()
 	visualizer.begin(Mesh.PRIMITIVE_LINE_STRIP)
-	
+
 	for vertex in bezier.get_baked_points():
 		visualizer.add_vertex(vertex)
-	
+
 	visualizer.end()
