4fe77f8494231854c7295c5b9774348a49556582
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-03-20 19:19:27 +0100

    A few (mostly name) changes so the paths can be found more easily.

--- testing/InteractableObjects/Compass/Compass.gd
+++ testing/InteractableObjects/Compass/Compass.gd
@@ -10,10 +10,10 @@ onready var compass_symbol = get_node("Spatial")
 
 func _process(delta):
 	if _is_picked_up:
-		var compass_plate_plane = Plane(controller.global_transform.basis.y, 0)
+		var compass_plate_plane = Plane(object_interaction.global_transform.basis.y, 0)
 		
 		var new_forward = compass_plate_plane.project(Vector3.FORWARD).normalized()
-		var new_up = controller.global_transform.basis.y
+		var new_up = object_interaction.global_transform.basis.y
 		var new_right = new_forward.cross(new_up)
 		
 		compass_symbol.global_transform.basis = Basis(new_right, new_up, -new_forward)
