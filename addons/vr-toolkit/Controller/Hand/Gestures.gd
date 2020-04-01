9e1cdbb357f1a379be699060f8b4833cce948107
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-01 20:48:11 +0200

    Fully functional gesture system although this is kind of redundant as the great @BastiaanOlij finished his skeleton support in the official godot open_vr repo.

--- addons/vr-toolkit/Controller/Hand/Gestures.gd
+++ addons/vr-toolkit/Controller/Hand/Gestures.gd
@@ -6,54 +6,47 @@ export(int, "any", "left", "right") var controller_side setget set_controller_si
 onready var anim = get_node("HandDefault/AnimationPlayer")
 onready var hand = get_node("HandDefault")
 
-var pointing: bool = false
-var grabbing: bool = false
-var thumb_on_ax: bool = false
+enum FINGER_POSITION{
+	pressed,
+	touched,
+	released
+}
+
+enum FINGERS{
+	thumb,
+	index,
+	middleRingPinky
+}
 
 
 func _ready():
-	$Inputs/PointingInput.connect("pressed", self, "set_pointing", [true])
-	$Inputs/PointingInput.connect("released", self, "set_pointing", [false])
-	$Inputs/GrabbingInput.connect("pressed", self, "set_grabbing", [true])
-	$Inputs/GrabbingInput.connect("released", self, "set_grabbing", [false])
-	$Inputs/ThumbInput.connect("pressed", self, "set_thumb", [false])
-	$Inputs/ThumbInput.connect("released", self, "set_thumb", [true])
+	$Inputs/PointingTouchInput.connect("pressed", self, "apply_gesture", [FINGERS.index, FINGER_POSITION.touched])
+	$Inputs/PointingTouchInput.connect("released", self, "apply_gesture", [FINGERS.index, FINGER_POSITION.released])
+	$Inputs/PointingPressInput.connect("pressed", self, "apply_gesture", [FINGERS.index, FINGER_POSITION.pressed])
+	$Inputs/PointingPressInput.connect("released", self, "apply_gesture", [FINGERS.index, FINGER_POSITION.touched])
+	$Inputs/GrabbingTouchInput.connect("pressed", self, "apply_gesture", [FINGERS.middleRingPinky, FINGER_POSITION.touched])
+	$Inputs/GrabbingTouchInput.connect("released", self, "apply_gesture", [FINGERS.middleRingPinky, FINGER_POSITION.released])
+	$Inputs/GrabbingPressInput.connect("pressed", self, "apply_gesture", [FINGERS.middleRingPinky, FINGER_POSITION.pressed])
+	$Inputs/GrabbingPressInput.connect("released", self, "apply_gesture", [FINGERS.middleRingPinky, FINGER_POSITION.touched])
+	$Inputs/ThumbTouchInput.connect("pressed", self, "apply_gesture", [FINGERS.thumb, FINGER_POSITION.touched])
+	$Inputs/ThumbTouchInput.connect("released", self, "apply_gesture", [FINGERS.thumb, FINGER_POSITION.released])
+	$Inputs/ThumbPressInput.connect("pressed", self, "apply_gesture", [FINGERS.thumb, FINGER_POSITION.pressed])
+	$Inputs/ThumbPressInput.connect("released", self, "apply_gesture", [FINGERS.thumb, FINGER_POSITION.touched])
 
 
-func apply_gesture():
-	if pointing:
-		anim.play("Point")
-		anim.advance(1)
+func apply_gesture(finger_id: int, finger_position: int):
+	if finger_position == FINGER_POSITION.pressed:
+		anim.set_current_animation(String(finger_id))
+		anim.seek(0.8333, true)
+		anim.stop(false)
+	elif finger_position == FINGER_POSITION.touched:
+		anim.set_current_animation(String(finger_id))
+		anim.seek(0.5, true)
+		anim.stop(false)
 	else:
-		anim.play("Point")
-		anim.advance(-1)
-	if grabbing:
-		anim.play("Grab")
-		anim.advance(1)
-	else:
-		anim.play("Grab")
-		anim.advance(-1)
-	if thumb_on_ax:
-		anim.play("Thumb", -1, 1.0, true)
-		anim.advance(-1)
-	else:
-		anim.play("Thumb", -1, 1.0, true)
-		anim.advance(1)
-
-
-func set_pointing(is_pointing):
-	pointing = is_pointing
-	apply_gesture()
-
-
-func set_grabbing(is_grabbing):
-	grabbing = is_grabbing
-	apply_gesture()
-
-
-func set_thumb(thumb_on):
-	thumb_on_ax = thumb_on
-	apply_gesture()
+		anim.set_current_animation(String(finger_id))
+		anim.seek(0.0, true)
+		anim.stop(false)
 
 
 # If left, mirror scene
