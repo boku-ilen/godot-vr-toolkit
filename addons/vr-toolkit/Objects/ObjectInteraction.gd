a429eab70763a7718d80e9bd63653c635c01fd8d
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-16 12:50:35 +0200

    Applied the new system on the object interaction.

--- addons/vr-toolkit/Objects/ObjectInteraction.gd
+++ addons/vr-toolkit/Objects/ObjectInteraction.gd
@@ -1,14 +1,17 @@
 extends "res://addons/vr-toolkit/Controller/ControllerTool.gd"
 
+export(int) var velocity_samples = 20
 
 onready var area = get_node("Area")
 
 # The picked up object
-var current_object: InteractableObject = null
+var current_object = null
 # The initial transform of the object when picking up
 var original_object_transform: Transform = Transform.IDENTITY
 # The initial node the object was child of
 var original_parent: Spatial = null
+# The initial state of showing controller/hand mesh
+var original_meshes: Dictionary
 var last_position = Vector3(0.0, 0.0, 0.0)
 var velocities = Array()
 
@@ -22,16 +25,9 @@ func _ready():
 
 func _process(delta):
 	velocities.push_back((global_transform.origin - last_position) / delta)
-	if velocities.size() > 10:
+	if velocities.size() > velocity_samples:
 		velocities.pop_front()
-	
 	last_position = global_transform.origin
-	# If we are currently holding an object we will translate it with the controller
-	if current_object:
-		if not current_object.fixed_position:
-			current_object.global_transform.origin = global_transform.origin
-			# Apply the rotation since the object has been picked up
-			current_object.global_transform.basis = global_transform.basis * original_object_transform.basis
 
 
 func on_interact(pressed):
@@ -48,34 +44,70 @@ func on_pickup(pressed: bool):
 	if pressed:
 		current_object = _try_pick_up_closest_interactable()
 		if not current_object == null:
-			# Only if fixed position of the object is not enabled.
-			if not current_object.fixed_position:
-				# Save the transform of the object when it was taken, we need this 
-				# to accurately compute the transform with the rotation of the controller
-				original_object_transform = current_object.global_transform
-				# Reset the basis of global_transform to Identity so only the rotation
-				# from the moment it has been picked up will be applied, not the rotation
-				# that the controller already has when picking up
-				global_transform.basis = Basis.IDENTITY
-			else:
-				global_transform.basis = get_parent().global_transform.basis
-				current_object.original_parent.remove_child(current_object)
-				current_object.mode = RigidBody.MODE_KINEMATIC
-				add_child(current_object)
-				current_object.transform = transform * current_object.position_in_hand
+			# If not static object
+			if current_object.get_class() == "VRInteractable":
+				pick_up()
+			elif current_object.get_class() == "VRStaticInteractable":
+				current_object.pick(controller_id, self)
 	else:
 	# If we are no longer holding the object we will call for its dropped method 
 	# and set the current_object to null
 		if current_object:
-			if current_object.fixed_position:
-				var glob_transform = current_object.global_transform
-				remove_child(current_object)
-				current_object.original_parent.add_child(current_object)
-				current_object.global_transform = glob_transform
-				current_object.mode = RigidBody.MODE_RIGID
+			if current_object.get_class() == "VRInteractable":
+				drop()
+				
+				# Show the initial meshes again
+				origin.set_show_meshes(controller_id, original_meshes.hand, original_meshes.controller)
+			elif current_object.get_class() == "VRStaticInteractable":
+				current_object.omitted()
 			
-			current_object.dropped(_get_velocity())
 			current_object = null
+
+
+func pick_up():
+	var original_object_basis = current_object.global_transform.basis
+	
+	if not current_object.is_picked_up:
+		current_object.original_parent.remove_child(current_object)
+		current_object.mode = RigidBody.MODE_KINEMATIC
+		add_child(current_object)
+		current_object.picked_up(controller_id, self)
+	# If it is picked up but not an interactable with a fixed position we want to be able
+	# to remove it from the other controller and take it into the other to be able to
+	# rotate the object
+	elif not current_object.fixed_position:
+
+		current_object.object_interaction.remove_child(current_object)
+		current_object.object_interaction.current_object = null
+		current_object.mode = RigidBody.MODE_KINEMATIC
+		add_child(current_object)
+		current_object.picked_up(controller_id, self)
+	else:
+		return
+	
+	# If hide meshes is checked, hide hand and controller mesh, save original state for showing them again
+	original_meshes = origin.get_show_meshes(controller_id)
+	origin.set_show_meshes(controller_id, current_object.show_controller_hand_meshes, current_object.show_controller_hand_meshes)
+	
+	current_object.global_transform.origin = global_transform.origin
+	
+	if not current_object.fixed_position:
+		# As the object should not have a fixed position, reassign the transform from before 
+		current_object.global_transform.basis = original_object_basis
+	else:
+		# If it has a fixed position reset the transform, so it does have the same position
+		global_transform.basis = get_parent().global_transform.basis
+		current_object.transform = transform * current_object.position_in_hand
+
+
+func drop():
+	var glob_transform = current_object.global_transform
+	remove_child(current_object)
+	current_object.original_parent.add_child(current_object)
+	current_object.global_transform = glob_transform
+	current_object.mode = RigidBody.MODE_RIGID
+	
+	current_object.dropped(_get_velocity())
 
 
 func _try_pick_up_closest_interactable():
@@ -88,8 +120,7 @@ func _try_pick_up_closest_interactable():
 			if distance < closest_distance:
 				closest_distance = distance
 				closest_body = body
-	
-	if not closest_body == null: closest_body.picked_up(controller_id, self)
+
 	return closest_body
 
 
