a9ff1c675a53ff2c5f19fe4275b346b5bdc08e14
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-16 13:51:45 +0200

    Made it possible to enable and disable the uifinger.

--- addons/vr-toolkit/Gui/GuiFinger.gd
+++ addons/vr-toolkit/Gui/GuiFinger.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+export(bool) var enabled = true
 
 onready var area = get_node("Area")
 onready var finger_tip = get_node("FingerTip")
@@ -14,18 +15,21 @@ func _ready():
 
 
 func _process(delta):
-	if current_gui:
-		current_gui.ray_interaction_input(
-				finger_tip.global_transform.origin, InputEventMouseMotion, on_hand)
+	if enabled:
+		if current_gui:
+			current_gui.ray_interaction_input(
+					finger_tip.global_transform.origin, InputEventMouseMotion, on_hand)
 
 
 func _on_touching(body=null):
-	if body.get_parent().has_method("ray_interaction_input"):
-		current_gui = body.get_parent()
-		current_gui.ray_interaction_input(
-				finger_tip.global_transform.origin, InputEventMouseButton, on_hand, true)
+	if enabled:
+		if body.get_parent().has_method("ray_interaction_input"):
+			current_gui = body.get_parent()
+			current_gui.ray_interaction_input(
+					finger_tip.global_transform.origin, InputEventMouseButton, on_hand, true)
 
 
 func _on_left(body=null):
+	if enabled:
 		current_gui.ray_interaction_input(
 				finger_tip.global_transform.origin, InputEventMouseButton, on_hand, false)
