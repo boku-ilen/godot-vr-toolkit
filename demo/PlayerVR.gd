b6fa37bd7d18faac57e6d6ed47da0b21a9b03bcc
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-16 12:52:40 +0200

    Small refactor and ability to easily toggle controller/hand-mesh visibility from another node.

--- demo/PlayerVR.gd
+++ demo/PlayerVR.gd
@@ -8,10 +8,10 @@ extends "res://addons/godot-openvr/scenes/ovr_main.gd"
 
 export(PackedScene) var vr_menu
 export(PackedScene) var objects_menu
-export(bool) var enable_controller_left setget set_enable_controller_left
-export(bool) var enable_controller_right setget set_enable_controller_right
-export(bool) var enable_hand_left setget set_enable_hand_left
-export(bool) var enable_hand_right setget set_enable_hand_right
+export(bool) var show_controller_left setget set_show_controller_left
+export(bool) var show_controller_right setget set_show_controller_right
+export(bool) var show_hand_left setget set_show_hand_left
+export(bool) var show_hand_right setget set_show_hand_right
 
 onready var controller_left = get_node("LeftVisual")
 onready var controller_right = get_node("RightVisual")
@@ -21,36 +21,52 @@ onready var hand_right = get_node("Right/Tip/Gestures")
 var interface
 
 
-func set_enable_controller_left(is_visible):
-	enable_controller_left = is_visible
-	get_node("LeftVisual").show_controller_mesh = enable_controller_left
+func set_show_controller_left(is_visible):
+	show_controller_left = is_visible
+	get_node("LeftVisual").show_controller_mesh = show_controller_left
 
 
-func set_enable_controller_right(is_visible):
-	enable_controller_right = is_visible
-	get_node("RightVisual").show_controller_mesh = enable_controller_right
+func set_show_controller_right(is_visible):
+	show_controller_right = is_visible
+	get_node("RightVisual").show_controller_mesh = show_controller_right
 
 
-func set_enable_hand_left(is_visible):
-	enable_hand_left = is_visible
-	get_node("Left/Tip/Gestures").visible = enable_hand_left
+func set_show_hand_left(is_visible):
+	show_hand_left = is_visible
+	get_node("Left/Tip/Gestures").visible = show_hand_left
 
 
-func set_enable_hand_right(is_visible):
-	enable_hand_right = is_visible
-	get_node("Right/Tip/Gestures").visible = enable_hand_right
+func set_show_hand_right(is_visible):
+	show_hand_right = is_visible
+	get_node("Right/Tip/Gestures").visible = show_hand_right
+
+
+func set_show_meshes(side: int, show_controller: bool, show_hand: bool):
+	if side == 1:
+		set_show_controller_left(show_controller)
+		set_show_hand_left(show_hand)
+	elif side == 2:
+		set_show_controller_right(show_controller)
+		set_show_hand_right(show_hand)
+
+
+func get_show_meshes(side: int):
+	if side == 1:
+		return {"hand": show_hand_left, "controller": show_controller_left}
+	elif side == 2:
+		return {"hand": show_hand_right, "controller": show_controller_right}
 
 
 func _ready():
-	set_enable_controller_left(enable_hand_left)
-	set_enable_controller_right(enable_hand_right)
-	set_enable_hand_left(enable_hand_left)
-	set_enable_hand_right(enable_hand_right)
+	set_show_controller_left(show_hand_left)
+	set_show_controller_right(show_hand_right)
+	set_show_hand_left(show_hand_left)
+	set_show_hand_right(show_hand_right)
 	GlobalVRAccess.controller_id_dict[controller_left.controller_id] = controller_left
 	GlobalVRAccess.controller_id_dict[controller_right.controller_id] = controller_right
 	
-#	init_object_menu()
-#	init_vr_menu()
+	init_object_menu()
+	init_vr_menu()
 
 
 func init_vr_menu():
