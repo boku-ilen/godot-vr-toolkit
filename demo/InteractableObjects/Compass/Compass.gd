507d759a3f03893d335aca40a9653aeb10c3775c
Mathias Baumgartinger <mathiasbaumgartinger@tutanota.com>
2021-08-10 16:22:15 +0200

    Cleaned up and documented some code in the compass-example.

--- demo/InteractableObjects/Compass/Compass.gd
+++ demo/InteractableObjects/Compass/Compass.gd
@@ -5,19 +5,25 @@ extends VRInteractable
 # Rotates with the latest global player position from PlayerInfo.
 #
 
-onready var compass_symbol = get_node("Spatial")
-onready var mesh = get_node("MeshInstance")
+# Obtain the required nodes.
+onready var compass_plate = get_node("CompassPlate")
+onready var compass_mesh = get_node("CompassMesh")
 
+# Initialize the transorm_before variable with the an identity matrix.
 var transform_before: Transform = Transform.IDENTITY
 
 func _process(delta):
 	if not transform_before == transform:
-		var compass_plate_plane = Plane(mesh.global_transform.basis.y, 0)
+		# Span a surface that takes the up direction of the mesh
+		var compass_plate_plane = Plane(compass_mesh.global_transform.basis.y, 0)
 		
+		# On to that plane project forward (0, 0, -1)
 		var new_forward = compass_plate_plane.project(Vector3.FORWARD).normalized()
-		var new_up = mesh.global_transform.basis.y
+		var new_up = compass_mesh.global_transform.basis.y
+		# The right direction for the compass will be computed via the cross product of the other two
 		var new_right = new_forward.cross(new_up)
 		
-		compass_symbol.global_transform.basis = Basis(new_right, new_up, -new_forward)
+		# Assign the new basis
+		compass_plate.global_transform.basis = Basis(new_right, new_up, -new_forward)
 	
 	transform_before = transform
